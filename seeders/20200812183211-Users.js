ee7c88becb7cc6f6bcc6a2e794302c311322823e
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-02-04 16:12:20 -0300

    commit argon2

--- seeders/20200812183211-Users.js
+++ seeders/20200812183211-Users.js
@@ -5,14 +5,16 @@ module.exports = {
         id: 1,
         display_name: 'Lewis Hamilton',
         email: 'lewishamilton@gmail.com',
-        password: '$argon2id$v=19$m=4096,t=3,p=1$LIM370Ee/r4EzpUQfAN5GA$90/J5chFWiwee3jstGksqdtMp3VN7Z93yEM8CNC2Uk8',
+        // password: '$argon2id$v=19$m=4096,t=3,p=1$LIM370Ee/r4EzpUQfAN5GA$90/J5chFWiwee3jstGksqdtMp3VN7Z93yEM8CNC2Uk8',
+        password: '123456',
         image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
       },
       {
         id: 2,
         display_name: 'Michael Schumacher',
         email: 'MichaelSchumacher@gmail.com',
-        password: '$argon2id$v=19$m=4096,t=3,p=1$Q6AQr5a4VHVWvW7tD9y1Xw$uTpiGzcIxSPxTpffOssIZL8rtCdct1Alc/aoaXpJbQ0',
+        //password: '$argon2id$v=19$m=4096,t=3,p=1$Q6AQr5a4VHVWvW7tD9y1Xw$uTpiGzcIxSPxTpffOssIZL8rtCdct1Alc/aoaXpJbQ0',
+        password: '123456',
         image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
       },
       ], { timestamps: false });
--- services/Users.js
+++ services/Users.js
@@ -1,4 +1,4 @@
-const argon = require('argon2');
+// const argon = require('argon2');
 const { generateToken } = require('../auth/login');
 const { Users } = require('../models');
 const UserValidator = require('../validations/User');
@@ -8,7 +8,8 @@ async function create({ displayName, email, password, image }) {
   if (errCode) return { errCode, message };
   const userWithEmail = await Users.findOne({ where: { email } });
   if (userWithEmail) return { errCode: 409, message: 'User already registered' };
-  const digest = await argon.hash(password, { type: argon.argon2id });
+  // const digest = await argon.hash(password, { type: argon.argon2id });
+  const digest = password;
   const user = await Users.create({ displayName, email, password: digest, image });
   const token = generateToken({ name: user.displayName, email: user.email });
   return { token };
@@ -19,7 +20,8 @@ async function login({ email, password }) {
   if (errCode) return { errCode, message };
   const user = await Users.findOne({ where: { email } });
   if (!user) return { errCode: 400, message: 'Invalid fields' };
-  const passwordValidation = await argon.verify(user.password, password, { type: argon.argon2id });
+  // const passwordValidation = await argon.verify(user.password, password, { type: argon.argon2id });
+  const passwordValidation = password === user.password;
   if (!passwordValidation) return { errCode: 400, message: 'Invalid fields' };
   const token = generateToken({ name: user.displayName, email: user.email });
   return { token };
