d2aa428653ccec30f2061ebd3d0024bb94284ad8
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-02-04 14:09:54 -0300

    Fix lint errors

--- models/blogposts.js
+++ models/blogposts.js
@@ -1,21 +1,23 @@
+const columns = (DataTypes) => ({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+  },
+  title: DataTypes.STRING,
+  content: DataTypes.STRING,
+  userId: DataTypes.INTEGER,
+  published: {
+    type: DataTypes.DATE,
+    field: 'created_at',
+  },
+  updated: {
+    type: DataTypes.DATE,
+    field: 'updated_at',
+  },
+});
+
 module.exports = (sequelize, DataTypes) => {
-  const BlogPosts = sequelize.define('BlogPosts', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-    },
-    title: DataTypes.STRING,
-    content: DataTypes.STRING,
-    userId: DataTypes.INTEGER,
-    published: {
-      type: DataTypes.DATE,
-      field: 'created_at',
-    },
-    updated: {
-      type: DataTypes.DATE,
-      field: 'updated_at',
-    },
-  }, {
+  const BlogPosts = sequelize.define('BlogPosts', columns(DataTypes), {
     underscored: true,
     tableName: 'BlogPosts',
   });
