38d1b57260a1ac709c944c999636919b5189da02
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2022-02-07 20:01:39 -0300

    Add argon2

--- services/Users.js
+++ services/Users.js
@@ -1,4 +1,4 @@
-// const argon = require('argon2');
+const argon = require('argon2');
 const { generateToken } = require('../auth/login');
 const { Users } = require('../models');
 const { loginSchema, newUserSchema } = require('../schemas/Users');
@@ -8,8 +8,7 @@ async function create({ displayName, email, password, image }) {
   if (validator.error) return { errCode: 400, message: validator.error.message };
   const userWithEmail = await Users.findOne({ where: { email } });
   if (userWithEmail) return { errCode: 409, message: 'User already registered' };
-  // const digest = await argon.hash(password, { type: argon.argon2id });
-  const digest = password;
+  const digest = await argon.hash(password, { type: argon.argon2id });
   const user = await Users.create({ displayName, email, password: digest, image });
   const token = generateToken({ name: user.displayName, email: user.email });
   return { token };
@@ -20,8 +19,7 @@ async function login({ email, password }) {
   if (validator.error) return { errCode: 400, message: validator.error.message };
   const user = await Users.findOne({ where: { email } });
   if (!user) return { errCode: 400, message: 'Invalid fields' };
-  // const passwordValidation = await argon.verify(user.password, password, { type: argon.argon2id });
-  const passwordValidation = password === user.password;
+  const passwordValidation = await argon.verify(user.password, password, { type: argon.argon2id });
   if (!passwordValidation) return { errCode: 400, message: 'Invalid fields' };
   const token = generateToken({ id: user.id, name: user.displayName, email: user.email });
   return { token };
