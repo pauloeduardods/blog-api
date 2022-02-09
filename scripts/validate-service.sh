92d1701dfe5a20afecdcb06f79a5294454a08ff9
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2022-02-09 13:43:54 -0300

    typo

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,6 +1,6 @@
 echo "Checking if application is running"
 
-if [ "$(docker container inspect -f '{{.State.Status}}' blog-api-backend-1)" == "running" ] && [ "$(docker container inspect -f '{{.State.Status}}' blog-api-db-1)" == "running" ]; then
+if [ "$(docker container inspect -f '{{.State.Status}}' blog-api_backend_1)" == "running" ] && [ "$(docker container inspect -f '{{.State.Status}}' blog-api_db_1)" == "running" ]; then
   echo "Application is running"
   docker container prune -f
   docker image prune -fa
